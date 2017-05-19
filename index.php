fbbdadc3e66729574754277669a2f4f059dbd276
calvincalvo <ccalvo916@gmail.com>
2017-05-18 17:00:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Products</title>
+    <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="shop-homepage.css" rel="stylesheet">
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Muli,Hammersmith+One,Tauri' rel='stylesheet' type='text/css'>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+   <link rel="stylesheet" href="navstyles.css">
+</head>
+<body>
+<header>
+    <div class="topOfNav">
+        <a  href="http://calvincodes.com/protosite/index.php">
+            <img  class="logo" src="images/webLogo.png" alt="Logo" height="78" width="300" />
+        </a>
+        <a href="#">
+        	<img class="cart" src="images/assets/cart.png" alt="cart"/>
+        </a>
+    </div>
+	<div id="cssmenu">
+  <ul>
+      <li><a href="about.php">ABOUT</a></li>
+      <li><a href="product.php">PRODUCTS</a>
+          <ul class="submenu">
+           <li><a href="sweet.php">Sweet</a></li>
+           <li><a href="salty.php">Salty</a></li>
+           <li><a href="savory.php">Savory</a></li>
+           <li><a href="gifts.php">Giftsets/Tasters</a></li>
+        </ul>
+      </li>
+      <li><a href="support.php">SUPPORT</a></li>
+      <li><a href="nutri.php">NUTRITION</a></li>
+     <li><a href="blog.php">BLOG</a>
+         <ul class="submenu">
+           <li><a href="#">Cooking Instructions</a></li>
+           <li><a href="#">History</a></li>
+           <li><a href="#">Personal</a></li>
+        </ul>
+      </li>
+     <li><a href="contact.php">CONTACT</a>
+         <ul class="submenu">
+           <li><a href="#">Catering</a></li>
+           <li><a href="#">Schedule An interview</a></li>
+           <li><a href="#">Personal</a></li>
+        </ul>
+     </li>
+  </ul>
+</div>
+    </header>
+<!-- nav-->
+
+<!-- sidebar-->
+<aside class="sidenav">
+    <ul>
+      <li><a href="#">SWEET</a></li>
+      <li><a href="#">SALTY</a></li>
+      <li><a href="#">SAVORY</a></li>
+      <li><a href="#">GIFTS AND TASTERS</a></li>
+    </ul>
+</aside>
+<!-- sidebar-->
+
+<!-- maincontent-->
+<section class="cats">
+    <h2 class="pageID"><a href="sweet.php">Sweet</a></h2>
+		<div class="featured">
+			<a href="dest.php"><img class="product" src="images/pizza.jpg" alt="pizza"></a>
+			<h3 class="pname">Pizza</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/burgers.jpg" alt="burger"></a>
+			<h3 class="pname">Cheeseburger</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/hotdog.jpg" alt="hotdog"></a>
+			<h3 class="pname">Hotdog</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+        <h2 class="pageID"><a href="salty.php">Salty</a></h2>
+		<div class="featured">
+			<a href="dest.php"><img class="product" src="images/pizza.jpg" alt="pizza"></a>
+			<h3 class="pname">Pizza</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/burgers.jpg" alt="burger"></a>
+			<h3 class="pname">Cheeseburger</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/hotdog.jpg" alt="hotdog"></a>
+			<h3 class="pname">Hotdog</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+        <h2 class="pageID"><a href="savory.php">Savory</a></h2>
+		<div class="featured">
+			<a href="dest.php"><img class="product" src="images/pizza.jpg" alt="pizza"></a>
+			<h3 class="pname">Pizza</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/burgers.jpg" alt="burger"></a>
+			<h3 class="pname">Cheeseburger</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/hotdog.jpg" alt="hotdog"></a>
+			<h3 class="pname">Hotdog</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+        <h2 class="pageID"><a href="gifts.php">Gifts and Testers</a></h2>
+		<div class="featured">
+			<a href="dest.php"><img class="product" src="images/pizza.jpg" alt="pizza"></a>
+			<h3 class="pname">Pizza</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/burgers.jpg" alt="burger"></a>
+			<h3 class="pname">Cheeseburger</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+		<div class="featured">
+			<a href="dest.php"><img class="product"src="images/hotdog.jpg" alt="hotdog"></a>
+			<h3 class="pname">Hotdog</h3>
+			<p>Pizza is a tasy italian food. Made up of mozarella cheese and tomato sauce. it can have a thin or thick crust</p>
+            <p>Price: $15</p>
+            <a href="#"><img class="cta" src="images/atoc.png"></a>
+		</div>
+</section>
+<!-- maincontent-->
+<!-- footer-->
+ <div class="social">
+        <a href="#">
+            <img title="Twitter" alt="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter.png" width="35" height="35" />
+        </a>
+        <a href="#">
+            <img title="Pinterest" alt="Pinterest" src="https://socialmediawidgets.files.wordpress.com/2014/03/13_pinterest.png" width="35" height="35" />
+        </a>
+        <a href="#">
+    <img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
+        </a>
+        <a href="#">
+    <img title="LinkedIn" alt="LinkedIn" src="https://socialmediawidgets.files.wordpress.com/2014/03/07_linkedin.png" width="35" height="35" />
+        </a>
+        <a href="#">
+            <img title="Instagram" alt="RSS" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram.png" width="35" height="35" />
+        </a>
+    </div>
+<footer>
+    <div class="footnav">
+        <div>
+            <h3><a href="#">About</a></h3>
+            <h3><a href="#">Products</a></h3>
+        <ul>
+            <li><a href="#">leo vitae</a></li>
+            <li><a href="#">vitae auctor</a></li>
+            <li><a href="#">leo faucibus</a></li>
+            <li><a href="#">auctor faucibus</a></li>
+        </ul>
+        </div>
+
+        <div>
+        <h3><a href="#">Blog</a></h3>
+        <ul>
+            <li><a href="#">leo vitae</a></li>
+            <li><a href="#">vitae auctor</a></li>
+            <li><a href="#">leo faucibus</a></li>
+            <li><a href="#">auctor faucibus</a></li>
+        </ul>
+        <h3><a href="#">Support</a></h3>
+        </div>
+
+        <div>
+        <h3><a href="#">Nutrition</a></h3>
+        <h3><a href="#">Contact</a></h3>
+        <ul>
+            <li><a href="#">leo vitae</a></li>
+            <li><a href="#">vitae auctor</a></li>
+            <li><a href="#">leo faucibus</a></li>
+            <li><a href="#">auctor faucibus</a></li>
+        </ul>
+        </div>
+
+        <div>
+            <h2>Can't find what you're looking for? Search here!</h2>
+            <p class="search"><img class="micon" src="images/micon.png">Search</p>
+        </div>
+    </div>
+<div class="smol"><small>
+   &copy; 2017, All Rights Reserved ~ Authored by Calvin Calvo <!--
+   <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
+   <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>-->
+    <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+    </small></div>
+<!-- jQuery -->
+    <script src="scripts/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="scripts/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
