be7aa46ea5892dd7c3495f638d9f3bd9c4f12f85
calvincalvo <ccalvo916@gmail.com>
2017-06-06 17:57:06 -0700

    07 create template files for your theme
    1.You need to break your theme apart into the 1) The Document (header.php), 2) The Footer (footer.php), 3) The Sidebar (sidebar.php), 4) Index (index.php), 5) Pages (page.php), 6) Single Post (single.php), 7) The Static Front Page (front-page.php) template files.
    
    2.Tag each file with the file’s name so I can see them in action.

---
+++ footer.php
@@ -0,0 +1,73 @@
+<?php wp_footer(); ?>
+<!-- footer-->
+<div class="social">
+    <a href="#">
+        <img title="Twitter" alt="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter.png" width="35" height="35" />
+    </a>
+    <a href="#">
+        <img title="Pinterest" alt="Pinterest" src="https://socialmediawidgets.files.wordpress.com/2014/03/13_pinterest.png" width="35" height="35" />
+    </a>
+    <a href="#">
+<img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
+    </a>
+    <a href="#">
+<img title="LinkedIn" alt="LinkedIn" src="https://socialmediawidgets.files.wordpress.com/2014/03/07_linkedin.png" width="35" height="35" />
+    </a>
+    <a href="#">
+        <img title="Instagram" alt="RSS" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram.png" width="35" height="35" />
+    </a>
+</div>
+<footer>
+<div class="footnav">
+    <div>
+        <h3><a href="#">About</a></h3>
+        <h3><a href="#">Products</a></h3>
+    <ul>
+        <li><a href="#">leo vitae</a></li>
+        <li><a href="#">vitae auctor</a></li>
+        <li><a href="#">leo faucibus</a></li>
+        <li><a href="#">auctor faucibus</a></li>
+    </ul>
+    </div>
+
+    <div>
+    <h3><a href="#">Blog</a></h3>
+    <ul>
+        <li><a href="#">leo vitae</a></li>
+        <li><a href="#">vitae auctor</a></li>
+        <li><a href="#">leo faucibus</a></li>
+        <li><a href="#">auctor faucibus</a></li>
+    </ul>
+    <h3><a href="#">Support</a></h3>
+    </div>
+
+    <div>
+    <h3><a href="#">Nutrition</a></h3>
+    <h3><a href="#">Contact</a></h3>
+    <ul>
+        <li><a href="#">leo vitae</a></li>
+        <li><a href="#">vitae auctor</a></li>
+        <li><a href="#">leo faucibus</a></li>
+        <li><a href="#">auctor faucibus</a></li>
+    </ul>
+    </div>
+
+    <div>
+        <h2>Can't find what you're looking for? Search here!</h2>
+        <p class="search"><img class="micon" src="<?php bloginfo('template_directory'); ?>/images/micon.png">Search</p>
+    </div>
+</div>
+<div class="smol"><small>
+&copy; 2017, All Rights Reserved ~ Authored by Calvin Calvo <!--
+<a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
+<a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>-->
+<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+</small></div>
+<!-- jQuery -->
+<script src="scripts/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="scripts/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
