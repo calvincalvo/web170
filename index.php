b2270db2238123c7c6aa83d0e565d9f296f9aa25
calvincalvo <ccalvo916@gmail.com>
2017-06-06 17:19:45 -0700

    06 creating main and sub menu systems
    *note main navigation was submitted with assignment 5*

--- index.php
+++ index.php
@@ -63,11 +63,24 @@
 <?php wp_head();?>
 <!-- sidebar-->
 <aside class="sidenav">
-    <ul>
-      <li><a href="#">SWEET</a></li>
-      <li><a href="#">SALTY</a></li>
-      <li><a href="#">SAVORY</a></li>
-      <li><a href="#">GIFTS AND TASTERS</a></li>
+	<h2><?php
+	if(is_page()) {
+		echo get_the_title($post->post_parent);
+	}else {
+		echo 'Blog';
+	}
+	?></h2>
+    <ul><?php
+	if(is_page()) {
+		if($post->post_parent){
+			wp_list_pages(array('child_of' => $post->post_parent, "title_li" => "",));
+		}else {
+			wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',));
+		}
+	}else {
+		wp_list_categories(array('title_li' => '',));
+	}
+	?>
     </ul>
 </aside>
 <!-- sidebar-->
