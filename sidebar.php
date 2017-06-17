d4c5992a163e4cfe12baef2a402fdbdc9d7ebfe6
calvincalvo <ccalvo916@gmail.com>
2017-06-16 23:03:59 -0700

    Add files via upload
    styled featured images on posts page/excerpts
    made alterations to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,30 +1,28 @@
 
-<aside class="sidenav">
-
+<div class="sidenav aside" id="sidebar">
 	<div id="sub-navigation" class="widget">
-	<h2><?php
-	if(is_page()) {
-		echo get_the_title($post->post_parent);
-	}else {
-		echo 'Blog';
-	}
-	?></h2>
-    <ul><?php
-	if(is_page()) {
-		if($post->post_parent){
-			wp_list_pages(array('child_of' => $post->post_parent, "title_li" => "",));
+		<h2><?php
+		if(is_page()) {
+			echo get_the_title($post->post_parent);
 		}else {
-			wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',));
-		}
-	}else {
-		wp_list_categories(array('title_li' => '',));
-	}
-	?>
-</ul>
+			echo 'Blog';
+		}?>
+		</h2>
+		<ul>
+			<?php
+			if(is_page()) {
+				if($post->post_parent){
+					wp_list_pages(array('child_of' => $post->post_parent, "title_li" => "",));
+				}else {
+					wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',));
+				}
+			}else {
+				wp_list_categories(array('title_li' => '',));
+			}?>
+		</ul>
 	<?php if (!(is_page())); ?>
 	<?php wp_list_categories(array('title_li' => __(' '))); ?>
-
-</div>
+	</div>
 	<?php if (get_post_meta($post->ID, 'quote', true)) ; ?>
 	<?php dynamic_sidebar(1); ?>
 	<div id="pull-quote" class="widget">
@@ -35,4 +33,4 @@
 	<?php dynamic_sidebar(1); ?>
 	<?php dynamic_sidebar(2); ?>
 	<?php dynamic_sidebar('fred'); ?>
-</aside>
+</div>
